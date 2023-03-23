7717b7039045eecef624d83ae85fac5cad7ebbdb
Estlib <75094683+Estlib@users.noreply.github.com>
2023-03-23 14:09:34 +0200

    initial project commit

---
+++ SolarSystemMobile/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SolarSystemMobile;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SolarSystemMobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SolarSystemMobile;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
