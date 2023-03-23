2a438f98f9de8e4ede2bfff532089402eb458d37
Estlib <75094683+Estlib@users.noreply.github.com>
2023-03-23 15:58:37 +0200

    added 3 planets

--- SolarSystemMobile/App.xaml.cs
+++ SolarSystemMobile/App.xaml.cs
@@ -1,11 +1,32 @@
-﻿namespace SolarSystemMobile;
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
 
+namespace SolarSystemMobile;
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    const int WindowWidth = 540;
+    const int WindowHeight = 1000;
+    public App()
+    {
+        InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+#if WINDOWS
+        Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+        {
+            var mauiWindow = handler.VirtualView;
+            var nativeWindow = handler.PlatformView;
+            nativeWindow.Activate();
+            IntPtr windowhandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+            WindowId windowId = Win32Interop.GetWindowIdFromWindow(windowhandle);
+            AppWindow appWindow = AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+        });
+
+#endif
+
+        MainPage = new NavigationPage(new StartPage());
+    }
 }
